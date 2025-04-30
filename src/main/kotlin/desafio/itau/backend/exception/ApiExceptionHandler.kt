00039e2f54044dd46ee5381361d885749fa53659
Gabriel Chioquetta <chioquetta@jornadatecnologia.com>
2025-04-30 10:22:30 -0300

    Fix: ajustes em exception

--- src/main/kotlin/desafio/itau/backend/exception/ApiExceptionHandler.kt
+++ src/main/kotlin/desafio/itau/backend/exception/ApiExceptionHandler.kt
@@ -2,19 +2,25 @@ package desafio.itau.backend.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 @ControllerAdvice
 class ApiExceptionHandler {
 
     @ExceptionHandler(InvalidTransactionException::class)
     fun handleInvalidTransaction(): ResponseEntity<Void> {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build()
+        return ResponseEntity.unprocessableEntity().build()
     }
 
-    @ExceptionHandler(Exception::class)
-    fun handleBadRequest(): ResponseEntity<Void> {
+    @ExceptionHandler(
+        MethodArgumentNotValidException::class,
+        MethodArgumentTypeMismatchException::class,
+        IllegalArgumentException::class
+    )
+    fun handleValidationError(): ResponseEntity<Void> {
         return ResponseEntity.badRequest().build()
     }
 }
